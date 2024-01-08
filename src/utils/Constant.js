288abf30da78b64422100caa274426553d8ee791
jaffrantirta <jaffrantirta@icloud.com>
2024-01-08 18:32:54 +0800

    update

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -1,8 +1,8 @@
-export const APP_NAME = "Wayan Geredeg";
-export const TAG_LINE = "I Wayan Geredeg.SH";
-export const SUPABASE_URL = "https://pxvxdxdwyfrqibwggwqe.supabase.co/";
+export const APP_NAME = "Mustafam Radja";
+export const TAG_LINE = "Mustafamradja";
+export const SUPABASE_URL = "https://jenhgfbnkucgybrznjgw.supabase.co";
 export const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4dnhkeGR3eWZycWlid2dnd3FlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDM2MDYwMjgsImV4cCI6MjAxOTE4MjAyOH0.TMXFq-ywJfzj-qXv7utGOCUT2hGTywPLXWDPkxhGIms";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImplbmhnZmJua3VjZ3licnpuamd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQzMzExNzgsImV4cCI6MjAxOTkwNzE3OH0.xUR_CrLXot1zbGD5iAnn4utWYNgcPS-oDqSRjS4nQr8";
 export const ERROR_MESSAGE = "Ups! Sepertinya ada kesalahan";
 export const IS_LOGIN =
   localStorage.getItem("access_token") !== null ? true : false;
